3d3829ee31ee4eb3fe514bfccd8944446c965c4a
Coen Valk <clcjvalk@gmail.com>
2020-03-20 11:29:03 -0400

    Quarantine 2020 update

---
+++ public/js/dynamicNavBar.js
@@ -0,0 +1,8 @@
+function toggleNavBar() {
+    var x = document.getElementById("nav");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
+}
